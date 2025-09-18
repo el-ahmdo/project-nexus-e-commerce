580d281389ebb5f1178ca9b4b3c775302fba9dad
el-ahmdo <aliyua875@gmail.com>
2025-09-18 20:43:27 +0100

    set up project structure with React and TypeScript and tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-amber-200 bg-amber-800 text-center p-6">
+        Vite + React
+      </h1>
+    </>
+  );
+}
+
+export default App;
